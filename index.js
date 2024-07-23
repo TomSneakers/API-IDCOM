e8b9d574ee668d05104f9bc96e615df776fe1a72
Tom <tomdesvignes031@gmail.com>
2024-07-23 15:17:32 +0200

    maj

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const port = process.env.PORT;
 app.use(express.json());
 app.use(cors()); // Assurez-vous d'utiliser cors ici
 
-mongoose.connect(process.env.MONGODB_URI)
+mongoose.connect("mongodb+srv://tomdesvignes031:Quentind31@cluster0.nlxrghj.mongodb.net/")
     .then(() => {
         console.log('Connected to MongoDB');
     })
