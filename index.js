032110afd77762f7c5ff2a9898ec57d29bf37c5a
Tom <tomdesvignes031@gmail.com>
2024-07-23 16:00:13 +0200

    maj

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ const sendNotification = async (title, message) => {
         console.error('Error preparing notifications:', error);
     }
 };
-app.use('/home', () => {
+app.use('/api/home', () => {
     return res.send('Welcome to the home page');
 }
 );
