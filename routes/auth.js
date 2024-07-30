8069aab6fe1edcbb1ebfeb3cadb4b6c614f326c7
Tom <tomdesvignes031@gmail.com>
2024-07-31 00:24:03 +0200

    maj

--- routes/auth.js
+++ routes/auth.js
@@ -1,31 +1,14 @@
 const express = require('express');
+const jwt = require('jsonwebtoken');
+const { Expo } = require('expo-server-sdk');
 const User = require('../models/User');
-const Token = require('../models/token');
+const Token = require('../models/Token');
 const { authenticateToken, checkRole } = require('../middleware/auth');
 
 const router = express.Router();
 const JWT_SECRET = process.env.JWT_SECRET || 'your_jwt_secret';
 
-// Signup
-router.post('/api/signup', async (req, res) => {
-    const { username, password } = req.body;
-    const user = new User({ username, password });
-    await user.save();
-    res.status(201).json({ message: 'User created' });
-});
-
-// Login
-router.post('/api/login', async (req, res) => {
-    const { username, password } = req.body;
-    const user = await User.findOne({ username });
-    if (!user || !(await user.comparePassword(password))) {
-        return res.status(401).json({ error: 'Invalid username or password' });
-    }
-
-    const token = user.generateAuthToken();
-    res.json({ token });
-});
-
+// Route to add token
 router.post('/api/add-token', authenticateToken, async (req, res) => {
     const { token } = req.body;
     const userId = req.user.id;
@@ -45,6 +28,26 @@ router.post('/api/add-token', authenticateToken, async (req, res) => {
         console.error('Error adding token:', error);
         res.status(500).json({ error: 'Error adding token' });
     }
+});
+
+// Signup
+router.post('/api/signup', async (req, res) => {
+    const { username, password } = req.body;
+    const user = new User({ username, password });
+    await user.save();
+    res.status(201).json({ message: 'User created' });
+});
+
+// Login
+router.post('/api/login', async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username });
+    if (!user || !(await user.comparePassword(password))) {
+        return res.status(401).json({ error: 'Invalid username or password' });
+    }
+
+    const token = user.generateAuthToken();
+    res.json({ token });
 });
 
 // Protected route example
